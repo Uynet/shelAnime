29d6486587feab39277e91f05de6ded04942e9d9
uynet <highsate@gmail.com>
2019-02-10 12:59:21 +0900

    po

--- po.sh
+++ po.sh
@@ -1,34 +1,27 @@
 declare -A frame
 
- frame[0]=".ro   .(ˊ~ˋ*ɔ۶🌟"
- frame[1]=" nO   .(ˊ-ˋ*)۶=====⭐"
+ frame[0]=".ro   ｡c*ˊ~ˋ)/🌟"
+ frame[1]=" nO   ｡(*ˊ-ˋ)۶=====⭐"
+ frame[2]=" Noω  ｡C*ˊ~ˋ)۶=-==-=★"
+ frame[3]=" NoW  ｡(*ˊ~ˋ)۶--- ~-☆"
+ frame[4]=" Now  ｡(*ˊ-ˋ)۶ ~   ・x"
+ frame[5]=" Now  ｡(*ˊ-ˋ)و       ✦"
+ frame[6]=" Now  ｡(*-~-),       +"
+ frame[7]=" Now .(>⌄<*).        ."
 
- frame[2]=" Noω  .(ˊ~ˋ*]۶=-==-=★"
- frame[3]=" NoW  .(ˊ~ˋ*)۶--- ~-☆"
-
- frame[4]=" Now  .(ˊ-ˋ*)۶ ~   ・x"
- frame[5]=" Now  .(ˊ-ˋ*)۶       ✦"
-
- frame[6]=" Now  .(=~=*)و       +"
- frame[7]=" Now \(*>⌄<),        ."
-
- frame[8]="   🌟 ٩c*ˊ˘ˋ)。Io     ."
- frame[9]=" ⭐===٩(*ˊᗜˋ)。[Ooo"
-
-frame[10]="★=-- ٩C*ˊᗜˋ)。LoOOlho"
-frame[11]="x- ~ ٩(*ˊᗜˋ)。Loadｪη8"
-
-frame[12]="+    ٩(*ˊᗜˋ)。Loading"
-frame[13]=".    ٩(*ˊᗜˋ)。Loading"
-
-frame[14]="     ٩(*ˋᗜˊ). Loading"
-frame[15]="      ٩(>⌄<*)/Loading"
+ frame[8]="   🌟 \(ˊ˘ˋ*っ。Io     ."
+ frame[9]=" ⭐===٩(ˊᗜˋ*)。[Ooo"
+frame[10]="★=-- ٩(ˊᗜˋ*ɔ。LoOOIho"
+frame[11]="x- ~ ٩(ˊᗜˋ*)。Loαdｪη9"
+frame[12]="+    ٩(ˊᗜˋ*)。Loading"
+frame[13]=".    ٩(ˊᗜˋ*)。Loading"
+frame[14]="     ٩(-ᗜ-*)。Loading"
+frame[15]="      .(*>⌄<).Loading"
 #٩⌄ˋᗜˊ▿
 while :; do
   for (( i=0; i<${#frame[*]}; i++ )); do
     clear
-    echo "             \n\n\n\n\n\n\n\n\n\n
-    ${frame[i]}"
-      sleep 0.05
+    echo "${frame[i]}"
+    sleep 0.06
   done
 done
